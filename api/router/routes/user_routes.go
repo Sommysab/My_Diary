e9ef2bca784ee1cb097896957490f9325cf9fcbe
Sebastine Udeh <sommy@mail.com>
2022-02-06 20:28:26 +0100

    u

--- api/router/routes/user_routes.go
+++ api/router/routes/user_routes.go
@@ -1,34 +1,44 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
-
-	"backend/api/controllers"
 )
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!\\nYour HTTP request method is %s\\n", r.Method)
+	w.WriteHeader(http.StatusAccepted)
+}
 
 var usersRoutes = []Route{
 	Route{
-		URI:          "/api/users",
+		URI:          "/",
 		Method:       http.MethodGet,
-		Handler:      controllers.GetUsers,
+		Handler:      helloHandler,
 		AuthRequired: false,
 	},
-	Route{
-		URI:          "/api/users/{id}",
-		Method:       http.MethodGet,
-		Handler:      controllers.GetUser,
-		AuthRequired: false,
-	},
-	Route{
-		URI:          "/api/users/{id}",
-		Method:       http.MethodPut,
-		Handler:      controllers.UpdateUser,
-		AuthRequired: true,
-	},
-	Route{
-		URI:          "/api/users/{id}",
-		Method:       http.MethodDelete,
-		Handler:      controllers.DeleteUser,
-		AuthRequired: true,
-	},
+	// Route{
+	// 	URI:          "/api/users",
+	// 	Method:       http.MethodGet,
+	// 	Handler:      controllers.GetUsers,
+	// 	AuthRequired: false,
+	// },
+	// Route{
+	// 	URI:          "/api/users/{id}",
+	// 	Method:       http.MethodGet,
+	// 	Handler:      controllers.GetUser,
+	// 	AuthRequired: false,
+	// },
+	// Route{
+	// 	URI:          "/api/users/{id}",
+	// 	Method:       http.MethodPut,
+	// 	Handler:      controllers.UpdateUser,
+	// 	AuthRequired: true,
+	// },
+	// Route{
+	// 	URI:          "/api/users/{id}",
+	// 	Method:       http.MethodDelete,
+	// 	Handler:      controllers.DeleteUser,
+	// 	AuthRequired: true,
+	// },
 }
