7a32acb1a7b9440bf642e9748b5e1b73ad8b0a29
Sebastine Udeh <sommy@mail.com>
2022-02-07 00:58:24 +0100

    u

--- api/database/db.go
+++ api/database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"backend/config"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
@@ -9,6 +10,8 @@ import (
 
 // Connect to the DATABASE
 func Connect() (*gorm.DB, error) {
+	fmt.Println(config.DBURL)
+	fmt.Println(config.DBDRIVER)
 	db, err := gorm.Open(config.DBDRIVER, config.DBURL)
 	// db, err := gorm.Open("mysql", "docker:passwor1d@tcp(godockerDB)/godocker?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
