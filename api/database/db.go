22f89a16af05dd36a579cfe40da40f9a28f511dd
Sebastine Udeh <sommy@mail.com>
2022-02-06 23:16:15 +0100

    u

--- api/database/db.go
+++ api/database/db.go
@@ -1,13 +1,16 @@
 package database
 
 import (
+	"backend/config"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
 // Connect to the DATABASE
 func Connect() (*gorm.DB, error) {
-	db, err := gorm.Open("mysql", "docker:1_paWsword@tcp(godockerDB)/godocker") // gorm.Open(config.DBDRIVER, config.DBURL)
+	// db, err := gorm.Open("mysql", "docker:1_paWsword@tcp(godockerDB)/godocker")
+	db, err := gorm.Open(config.DBDRIVER, config.DBURL)
 	if err != nil {
 		return nil, err
 	}
--- config/config.go
+++ config/config.go
@@ -29,11 +29,11 @@ func Load() {
 		PORT = 9000
 	}
 	DBDRIVER = os.Getenv("DB_DRIVER")
-	DBURL = fmt.Sprintf("%s:%s@tcp(diary_db)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	DBURL = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		// os.Getenv("DB_HOST"),
-		// os.Getenv("DB_PORT"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
 
