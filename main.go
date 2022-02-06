a8750f186268f5f2538c6b83f9dfc8d83e3f0f3c
Sebastine Udeh <sommy@mail.com>
2022-02-06 14:57:05 +0100

    up

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: r,
-		Addr:    ":8000",
+		Addr:    ":80",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
