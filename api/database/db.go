a054b929628b29f07ae89112d4199f05da51754a
Sebastine Udeh <sommy@mail.com>
2022-02-06 22:54:22 +0100

    u

--- api/database/db.go
+++ api/database/db.go
@@ -7,7 +7,6 @@ import (
 
 // Connect to the DATABASE
 func Connect() (*gorm.DB, error) {
-	// db, err := gorm.Open("postgres", "host=blog port=5432 user=postgres dbname=postgres sslmode=disable password=postgres")
 	db, err := gorm.Open("mysql", "docker:password@tcp(godockerDB)/godocker") // gorm.Open(config.DBDRIVER, config.DBURL)
 	if err != nil {
 		return nil, err
