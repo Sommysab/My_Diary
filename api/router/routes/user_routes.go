b8f063bb4fd535b5b3a112b9887ea799c17b9729
Sebastine Udeh <sommy@mail.com>
2022-02-07 08:26:14 +0100

    up

--- api/router/routes/user_routes.go
+++ api/router/routes/user_routes.go
@@ -12,7 +12,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 var usersRoutes = []Route{
 	Route{
-		URI:          "/api/",
+		URI:          "/api",
 		Method:       http.MethodGet,
 		Handler:      helloHandler,
 		AuthRequired: false,
